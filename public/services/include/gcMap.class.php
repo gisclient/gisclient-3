18b68a856f36be5e336843512c445fbe5a491c53
Roberto Starnini <roberto.starnini@gmail.com>
2013-09-20 19:00:49 +0200

    fix resolutions cast

--- public/services/include/gcMap.class.php
+++ public/services/include/gcMap.class.php
@@ -867,7 +867,8 @@ class gcMap{
 	function _getResolutions($minScale,$maxScale,$sizeUnitId){
 
 		//156543.03390625,78271.516953125,39135.7584765625,19567.87923828125,9783.939619140625,4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677,0.14929107084870338,0.07464553542435169
-	
+				//Fattore di conversione tra dpi e unità della mappa
+		$convFact = GCAuthor::$aInchesPerUnit[$sizeUnitId]*MAP_DPI;
 		if(!$this->serverResolutions){
 			//se mercatore sferico setto le risoluzioni di google altrimenti uso quelle predefinite dall'elenco scale
 			$aRes = array();
@@ -877,23 +878,21 @@ class gcMap{
 			}
 			else{
 	            $scaleList = $this->_getScaleList();
-				foreach($scaleList as $scaleValue)	$aRes[] = $scaleValue/$this->conversionFactor;
+				foreach($scaleList as $scaleValue)	$aRes[] = $scaleValue/$convFact;
 			}
 			$this->serverResolutions = $aRes;
 		}
 
-		//Fattore di conversione tra dpi e unità della mappa
-		$convFact = GCAuthor::$aInchesPerUnit[$sizeUnitId]*MAP_DPI;
 		$minResIndex = count($this->serverResolutions);
 		$maxResIndex = 0;
 		if($minScale){
-			$res = floatval($minScale)/$convFact;
+			$res = (string)(floatval($minScale)/$convFact);
 			if(array_index($this->serverResolutions,$res)!==false)
 				$minResIndex = array_index($this->serverResolutions,$res);
 		}
 		
 		if($maxScale){
-			$res = floatval($maxScale)/$convFact;
+			$res = (string)(floatval($maxScale)/$convFact);
 			if(array_index($this->serverResolutions,$res)!==false)
 				$maxResIndex = array_index($this->serverResolutions,$res);
 		}
