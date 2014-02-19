0e660970ea7abd4f52039ecedefa2980a4139245
Roberto <robystar@vmserver.gisweb>
2014-02-19 12:13:54 +0100

    Add message no mapset

--- public/services/gcmap.php
+++ public/services/gcmap.php
@@ -24,24 +24,28 @@
 * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 *
 ******************************************************************************/
-if(empty($_REQUEST['mapset'])) die("alert('Manca il parametro mapset');");
 
 require_once('../../config/config.php');
 require_once (ADMIN_PATH."lib/functions.php");
 require_once (ROOT_PATH."lib/i18n.php");
 require_once ('include/gcMap.class.php');
 
+
+
 $getLegend = false;
 if(isset($_REQUEST['legend']) && $_REQUEST['legend'] == 1) $getLegend = true;
 $languageId = null;
 if(!empty($_REQUEST['lang'])) $languageId = $_REQUEST['lang'];
 
-$objMapset = new gcMap($_REQUEST["mapset"], $getLegend, $languageId);
 header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
 header ("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header ("Pragma: no-cache"); // HTTP/1.0
 header("Content-Type: application/json; Charset=UTF-8");
+
+if(empty($_REQUEST['mapset'])) die(json_encode(array('error' => 200, 'message' => 'No mapset name')));
+$objMapset = new gcMap($_REQUEST["mapset"], $getLegend, $languageId);
+
 if(empty($_GET["jsoncallback"]))
 	echo json_encode($objMapset->mapConfig);
 else
