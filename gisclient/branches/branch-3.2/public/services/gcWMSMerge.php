162510121172d3c39f8110ba7ecdf4e66ddef4e0
phopfgartner <phopfgartner@f1ac595a-1a33-490c-9e3d-5072c46764fd>
2014-04-17 09:26:09 +0000

    inelegant support for custom parameter PREV_TIME

--- gisclient/branches/branch-3.2/public/services/gcWMSMerge.php
+++ gisclient/branches/branch-3.2/public/services/gcWMSMerge.php
@@ -51,6 +51,7 @@ foreach($mapConfig['layers'] as $key => $layer) {
 		if(!empty($layer['PARAMETERS']['PROJECT'])) $url .= 'PROJECT='.$layer['PARAMETERS']['PROJECT'];
 		if(!empty($layer['PARAMETERS']['MAP'])) $url .= '&MAP='.$layer['PARAMETERS']['MAP'];
 		if(!empty($layer['PARAMETERS']['TIME'])) $url .= '&TIME='.$layer['PARAMETERS']['TIME'];
+        if(!empty($layer['PARAMETERS']['PREV_TIME'])) $url .= '&PREV_TIME='.$layer['PARAMETERS']['PREV_TIME'];
 		if(!empty($layer['PARAMETERS']['REDLINEID'])) $url .= '&REDLINEID='.$layer['PARAMETERS']['REDLINEID'];
         if(!empty($layer['PARAMETERS']['LANG'])) $url .= '&LANG='.$layer['PARAMETERS']['LANG'];
 		if(!empty($sessionId)) $url .= '&'.GC_SESSION_NAME.'='.$sessionId;
