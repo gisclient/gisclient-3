91f994bc8b809bba70c44fbc826032a656ffdc09
phopfgartner <phopfgartner@f1ac595a-1a33-490c-9e3d-5072c46764fd>
2014-08-21 07:41:03 +0000

    GCAjax was instantiated twice

--- gisclient/branches/branch-3.2/public/services/get1nData.php
+++ gisclient/branches/branch-3.2/public/services/get1nData.php
@@ -1,7 +1,6 @@
 <?php
-require_once('../../config/config.php');
+require_once '../../config/config.php';
 require_once ROOT_PATH.'lib/ajax.class.php';
-$ajax = new GCAjax();
 
 if(empty($_REQUEST['qtrelation_id'])) $ajax->error('Undefined qtrelation_id');
 if(empty($_REQUEST['f_key_value'])) $ajax->error('Undefined f_key_value');
@@ -35,7 +34,6 @@ $relationSchema = GCApp::getDataDBSchema($qtRelation['catalog_path']);
 $layerTable = $qtRelation['layer_table'];
 $relationTable = $qtRelation['table_name'];
 
-
 $sql = 'select '.implode(', ', $fieldsName).' from '.$layerSchema.'.'.$layerTable.' as t_0 left join '.$relationSchema.'.'.$relationTable.' as t_'.$qtRelation['qtrelation_id'].'  on t_'.$qtRelation['qtrelation_id'].'.'.$qtRelation['table_field_1'].' = t_0.'.$qtRelation['data_field_1'].' where t_0.'.$qtRelation['data_field_1'].' = :value';
 
 $stmt = $layerDataDb->prepare($sql);
@@ -45,6 +43,5 @@ $results = array(
     'fields'=>$fields,
     'results'=>$stmt->fetchAll(PDO::FETCH_ASSOC)
 );
-
 
 $ajax->success(array('data'=>$results));
