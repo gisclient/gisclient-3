93c9f4d74a18b59dcff44a602d8aec9ce516b632
phopfgartner <phopfgartner@f1ac595a-1a33-490c-9e3d-5072c46764fd>
2014-08-29 08:07:59 +0000

    filter data, so that null values are replaced with empty spaces. This makles the table look cleaner

--- gisclient/branches/branch-3.2/public/services/get1nData.php
+++ gisclient/branches/branch-3.2/public/services/get1nData.php
@@ -2,6 +2,22 @@
 require_once '../../config/config.php';
 require_once ROOT_PATH.'lib/ajax.class.php';
 
+/**
+ * Replace all null values with an emty string, so that JSON seraialization
+ * does not show 'null'
+ * 
+ * @param array $data array of array
+ */
+function replaceNullWithBlank(array &$data) {
+	foreach($data as $rowNum => $rowData) {
+		foreach($rowData as $colNum => $item) {
+			if (is_null($item)) {
+				$data[$rowNum][$colNum] = '';
+			}
+		}
+	}
+}
+
 if(empty($_REQUEST['qtrelation_id'])) $ajax->error('Undefined qtrelation_id');
 if(empty($_REQUEST['f_key_value'])) $ajax->error('Undefined f_key_value');
 
@@ -44,4 +60,5 @@ $results = array(
     'results'=>$stmt->fetchAll(PDO::FETCH_ASSOC)
 );
 
+replaceNullWithBlank($results['results']);
 $ajax->success(array('data'=>$results));
