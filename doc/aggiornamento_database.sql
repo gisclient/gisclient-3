c5db5bef8906a7595d7b46ae9d2246ebdbc77356
Peter Hopfgartner <peter.hopfgartner@r3-gis.com>
2014-11-04 14:08:15 +0100

    drop before rewriting view

--- doc/aggiornamento_database.sql
+++ doc/aggiornamento_database.sql
@@ -1305,7 +1305,8 @@ CREATE OR REPLACE VIEW vista_version AS
 INSERT INTO version (version_name,version_key, version_date) values ('3.2.22', 'author', '2014-08-26');
 
 -- 2014-10-01: crea la vista_layer, utile a sapere se un layer è interrogabile e/o editabile
- CREATE OR REPLACE VIEW vista_layer AS 
+DROP VIEW IF EXISTS vista_layer;
+CREATE OR REPLACE VIEW vista_layer AS 
  SELECT l.*, 
         CASE
           WHEN queryable = 1 and l.hidden = 0 and 
