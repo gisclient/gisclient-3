9623eaf6b53c1dc0f4709dcfcf28fea6195e5f79
Peter Hopfgartner <peter.hopfgartner@r3-gis.com>
2014-11-07 15:12:12 +0100

    fixes

--- tests/Web/LoginTest.php
+++ tests/Web/LoginTest.php
@@ -4,29 +4,40 @@ include_once __DIR__."/../../config/config.php";
 include_once __DIR__."/../lib/HttpUtils.php";
 
 class LoginTest extends PHPUnit_Framework_TestCase {
+	private $cookieJar;
+	
+	public function __construct() {
+		$this->cookieJar = GC_WEB_TMP_DIR.'test_cookies.txt';
+	}
 	
     public function testLoginFailure() {
-		$cookieJar = GC_WEB_TMP_DIR.'test_cookies.txt';
-		unlink($cookieJar);
+		
+		if (file_exists($this->cookieJar)) {
+			unlink($this->cookieJar);
+		}
+		
 		$postParameters = array(
 			'username' => 'admin',
 			'password' => 'wrongpassword',
 			'azione'   => 'Entra',
 		);
-		$loginResult = HttpUtils::post(PUBLIC_URL.'index.php', $postParameters, $cookieJar);
+		$loginResult = HttpUtils::post(PUBLIC_URL.'index.php', $postParameters, $this->cookieJar);
 		$this->assertEquals($loginResult[0], 200);
 		$this->assertTrue(strpos($loginResult[1], 'LogOut') === false);
     }
 	
     public function testLogin() {
-		$cookieJar = GC_WEB_TMP_DIR.'test_cookies.txt';
+		
+		if (file_exists($this->cookieJar)) {
+			unlink($this->cookieJar);
+		}
 		
 		$postParameters = array(
 			'username' => 'admin',
 			'password' => 'gisclient',
 			'azione'   => 'Entra',
 		);
-		$loginResult = HttpUtils::post(PUBLIC_URL.'index.php', $postParameters, $cookieJar);
+		$loginResult = HttpUtils::post(PUBLIC_URL.'index.php', $postParameters, $this->cookieJar);
 		$this->assertEquals($loginResult[0], 200);
 		$this->assertTrue(strpos($loginResult[1], 'LogOut') !== false);
     }
