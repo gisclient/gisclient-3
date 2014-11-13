d28f1da5095dc0b79834fa52b4fe04e6528f57aa
Peter Hopfgartner <peter.hopfgartner@r3-gis.com>
2014-11-13 09:50:50 +0100

    add clas skeleton for Pixmap handling

---
+++ public/admin/lib/PixmapSymbol.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Handle user definable user symbols
+ * 
+ */
+class PixmapSymbol {
+	
+	public function __construct() {
+		
+	}
+	
+	public function getList($sqlFilter = '') {
+		// filter by ILIKE '%PIXMAP%' and $sqlFilter
+	}
+	public function upload() {
+		// verify extension
+		
+		// verify is correct image
+		
+		// move to final destination
+		
+		// 
+		
+	}
+}
