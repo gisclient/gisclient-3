20e36bf4d433d53075120cb5e8de7614d8df0112
Marco Stevan <marco.stevan@r3-gis.com>
2015-08-06 14:27:07 +0200

    accept json data

--- public/services/print.php
+++ public/services/print.php
@@ -10,6 +10,11 @@ $gcService->startSession();
 
 $ajax = new GCAjax();
 
+$inputJSONText = file_get_contents('php://input');
+if (($data = json_decode($inputJSONText, true)) !== null) {
+    $_REQUEST = $data;
+}
+
 if ($_REQUEST['format'] == 'PDF') {
 	if(!file_exists(GC_FOP_LIB)) $ajax->error('fop lib does not exist');
 	require_once GC_FOP_LIB;
