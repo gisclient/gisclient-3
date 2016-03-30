cfd427cecc682093fb42188d47fdc43514758fa9
root <root@r3bzprd-kvm-app02.r3-gis.com>
2016-03-30 13:34:56 +0200

    Backward compatibility for sencha touch online map

---
+++ public/services/include/gcMap.class.r3gis.senchatouch.php
@@ -0,0 +1,91 @@
+<?php
+
+class gcMapSenchaTouchUtils
+{
+
+    /**
+     * Convert the standard mapOptions to SenchaTouch format
+     * *
+     * @param array $mapOptions   the $mapOptions
+     * @return array              the converted mapOptions
+     * @throws Exception
+     */
+    static function toSenchaTouch(array $mapOptions)
+    {
+        $themes = $mapOptions['theme'];
+        unset($mapOptions['theme']);
+
+        $olMapConfig = array();
+        foreach ($mapOptions as $key => $val) {
+            if (in_array($key, array(
+                    'units', 'projection', 'displayProjection', 'resolutions', 'minZoomLevel', 'maxResolution',
+                    'minResolution', 'maxExtent', 'restrictedExtent'))) {
+                $olMapConfig[$key] = $val;
+                unset($mapOptions[$key]);
+            }
+        }
+        $mapOptions['olMapConfig'] = $olMapConfig;
+        $layers = array();
+        foreach ($themes as $themeName => $theme) {
+            $lastThemeType = null;
+            foreach ($theme as $key => $val) {
+                if (in_array($key, array('title', 'radio'))) continue;
+                if ($lastThemeType === null) {
+                    $lastThemeType = $val['type'];
+                } else if ($lastThemeType <> $val['type']) {
+                    throw new Exception("Mixed layer types not allowed in mobile theme \"{$key}\" [From type {$lastThemeType} to type {$val['type']}]");
+                }
+            }
+            switch ($lastThemeType) {
+                case 1:
+                    $layer = array(
+                        'layerType' => 'WMS',
+                        'name' => $theme['title'],
+                        'url' => null,
+                        'parameters' => array(),
+                        'options' => array());
+
+                    $layerNames = array();
+                    foreach ($theme as $key => $val) {
+                        if (in_array($key, array('title', 'radio'))) continue;
+                        $layer['parameters'] = array_merge($layer['parameters'],
+                            $val['parameters']);
+                        unset($val['options']['featureTypes']);
+                        $layer['options']    = array_merge($layer['options'],
+                            $val['options']);
+                        $layer['url']        = $val['url'];
+                        if (!empty($val['parameters']['layers'])) {
+                            foreach ($val['parameters']['layers'] as $layerName)
+                                array_push($layerNames, $layerName);
+                        } else {
+                            continue;
+                        }
+                    }
+                    if (!empty($layerNames)) {
+                        $layer['parameters']['layers'] = $layerNames;
+                        array_push($layers, $layer);
+                    }
+                    break;
+                case 6:
+                    foreach ($theme as $key => $val) {
+                        if (in_array($key, array('title', 'radio'))) {
+                            continue;
+                        }
+                        unset($val['options']['serviceVersion']);
+                        $layer = array(
+                            'layerType' => 'TMS',
+                            'name' => $val['title'],
+                            'url' => $val['url'],
+                            'tileOrigin' => array($olMapConfig['restrictedExtent'][0],
+                                $olMapConfig['restrictedExtent'][1]),
+                            'layername' => $val['options']['layers']);
+                        $layer = array_merge($val['options'], $layer);
+                        array_push($layers, $layer);
+                    }
+                    break;
+            }
+        }
+        $mapOptions['layers'] = array_reverse($layers);
+        return $mapOptions;
+    }
+}
