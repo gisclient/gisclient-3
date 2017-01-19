ae2a17a271a9b0854b38ad1866dc7b14840d2f73
Work <marco.stevan@r3-gis.com>
2017-01-19 09:25:37 +0100

    calc scale if missing from viewport

--- public/services/download.php
+++ public/services/download.php
@@ -11,77 +11,85 @@ $gcService->startSession();
 
 $options = array('image_format'=>'gtiff', 'output_format'=>'geotiff');
 
-if($_REQUEST['format'] == 'png') {
-	$options['image_format'] = 'png';
-	$options['output_format'] = 'png';
-	$options['save_image'] = true;
-	$options['file_name'] = GCApp::getUniqueRandomTmpFilename(GC_WEB_TMP_DIR, 'gc_mapimage', 'png');
-} else if($_REQUEST['format'] == 'jpeg') {
-	$options['image_format'] = 'jpeg';
-	$options['output_format'] = 'jpeg';
-	$options['save_image'] = true;
-	$options['file_name'] = GCApp::getUniqueRandomTmpFilename(GC_WEB_TMP_DIR, 'gc_mapimage', 'jpeg');
+if ($_REQUEST['format'] == 'png') {
+    $options['image_format'] = 'png';
+    $options['output_format'] = 'png';
+    $options['save_image'] = true;
+    $options['file_name'] = GCApp::getUniqueRandomTmpFilename(GC_WEB_TMP_DIR, 'gc_mapimage', 'png');
+} else if ($_REQUEST['format'] == 'jpeg') {
+    $options['image_format'] = 'jpeg';
+    $options['output_format'] = 'jpeg';
+    $options['save_image'] = true;
+    $options['file_name'] = GCApp::getUniqueRandomTmpFilename(GC_WEB_TMP_DIR, 'gc_mapimage', 'jpeg');
 }
 
-if(!empty($_REQUEST['tiles']) && is_array($_REQUEST['tiles'])) {
-	$tiles = $_REQUEST['tiles'];
+if (!empty($_REQUEST['tiles']) && is_array($_REQUEST['tiles'])) {
+    $tiles = $_REQUEST['tiles'];
 } else {
-    print_debug('No tiles',null,'download');
-	die(json_encode(array('result' => 'error', 'error' => 'No tiles')));
+    print_debug('No tiles', null, 'download');
+    die(json_encode(array('result' => 'error', 'error' => 'No tiles')));
 }
 
-if(empty($_REQUEST['viewport_size']) || !is_array($_REQUEST['viewport_size']) || count($_REQUEST['viewport_size']) != 2) {
-    print_debug('No tiles',null,'size');
-	die(json_encode(array('result' => 'error', 'error' => 'No size')));
+if (empty($_REQUEST['viewport_size']) || !is_array($_REQUEST['viewport_size']) || count($_REQUEST['viewport_size']) != 2) {
+    print_debug('No tiles', null, 'size');
+    die(json_encode(array('result' => 'error', 'error' => 'No size')));
 }
 
-if(empty($_REQUEST['srid'])){
-    print_debug('No tiles',null,'No srid');
-	die(json_encode(array('result' => 'error', 'error' => 'No srid')));
+if (empty($_REQUEST['srid'])) {
+    print_debug('No tiles', null, 'No srid');
+    die(json_encode(array('result' => 'error', 'error' => 'No srid')));
 }
 $srid = $_REQUEST['srid'];
-if(strpos($_REQUEST['srid'], ':') !== false) {
-	list($options['auth_name'], $srid) = explode(':', $_REQUEST['srid']);
+if (strpos($_REQUEST['srid'], ':') !== false) {
+    list($options['auth_name'], $srid) = explode(':', $_REQUEST['srid']);
 }
 
-if(!empty($_REQUEST['scale_mode'])) $options['scale_mode'] = $_REQUEST['scale_mode'];
-if(!empty($_REQUEST['fixed_size'])) $options['fixed_size'] = $_REQUEST['fixed_size'];
-if(!empty($_REQUEST['scale'])) {
-	$options['scale'] = $_REQUEST['scale'];
-} else {
-	print_debug('missing mandatory parameter "scale"',null,'download');
-    die(json_encode(array('result' => 'error', 'error' => 'missing mandatory parameter "scale"')));
-}
-if(!empty($_REQUEST['center'])) $options['center'] = $_REQUEST['center'];
-$options['dpi'] = 96;
-if(!empty($_REQUEST['dpi']) && is_numeric($_REQUEST['dpi'])){
-	$options['dpi'] = (int) $_REQUEST['dpi'];
-	
+$options['dpi'] = MAP_DPI;
+if (!empty($_REQUEST['dpi']) && is_numeric($_REQUEST['dpi'])) {
+    $options['dpi'] = (int) $_REQUEST['dpi'];
 }
 
-if(!empty($_REQUEST['extent'])) {
-	$options['extent'] = explode(',', $_REQUEST['extent']);
+if (!empty($_REQUEST['extent'])) {
+    $options['extent'] = explode(',', $_REQUEST['extent']);
 } else {
-	// we could eventually try to reconstruct the extent from the viewport size, but is it worth to complicate the code?
-	print_debug('missing mandatory parameter "extent"',null,'download');
+    // we could eventually try to reconstruct the extent from the viewport size, but is it worth to complicate the code?
+    print_debug('missing mandatory parameter "extent"', null, 'download');
     die(json_encode(array('result' => 'error', 'error' => 'missing mandatory parameter "extent"')));
+}
+if (!empty($_REQUEST['scale_mode'])) {
+    $options['scale_mode'] = $_REQUEST['scale_mode'];
+}
+if (!empty($_REQUEST['fixed_size'])) {
+    $options['fixed_size'] = $_REQUEST['fixed_size'];
+}
+if (!empty($_REQUEST['scale'])) {
+    $options['scale'] = $_REQUEST['scale'];
+} else {
+    $scaleWidth = (int) $options['dpi'] * 100 / 2.54 * ($options['extent'][2] - $options['extent'][0]) / $viewport_size[0];
+    $scaleHeight = (int) $options['dpi'] * 100 / 2.54 * ($options['extent'][3] - $options['extent'][1]) / $viewport_size[1];
+
+    $options['scale'] = ($scaleWidth < $scaleHeight)? $scaleWidth : $scaleHeight;
+}
+
+if (!empty($_REQUEST['center'])) {
+    $options['center'] = $_REQUEST['center'];
 }
 
 $imageSize = array(
-	0 => (int) $options['dpi'] * 100 / 2.54 * ($options['extent'][2] - $options['extent'][0]) / $options['scale'],
-	1 => (int) $options['dpi'] * 100 / 2.54 * ($options['extent'][3] - $options['extent'][1]) / $options['scale'],
+    0 => (int) $options['dpi'] * 100 / 2.54 * ($options['extent'][2] - $options['extent'][0]) / $options['scale'],
+    1 => (int) $options['dpi'] * 100 / 2.54 * ($options['extent'][3] - $options['extent'][1]) / $options['scale'],
 );
 
-if(isset($_REQUEST['scalebar'])) {
-	$options['scalebar'] = $_REQUEST['scalebar'];
+if (isset($_REQUEST['scalebar'])) {
+    $options['scalebar'] = $_REQUEST['scalebar'];
 }
-			
+
 try {
-	$mapImage = new mapImage($tiles, $imageSize, $srid, $options);
-	$imageUrl = $mapImage->getImageUrl();
+    $mapImage = new mapImage($tiles, $imageSize, $srid, $options);
+    $imageUrl = $mapImage->getImageUrl();
 } catch (Exception $e) {
-    print_debug($e->getTraceAsString(),null,'download');
-    print_debug($e->getMessage(),null,'download');
+    print_debug($e->getTraceAsString(), null, 'download');
+    print_debug($e->getMessage(), null, 'download');
     die(json_encode(array('result' => 'error', 'error' => $e->getMessage())));
 }
 die(json_encode(array('result' => 'ok', 'file' => $imageUrl, 'format' => $options['output_format'])));
