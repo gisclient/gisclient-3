3f9d9e62327f5183e86f21ab30b9356d8676d3a1
Work <marco.stevan@r3-gis.com>
2017-01-19 11:54:21 +0100

    fix: set dinamic header and linting

--- public/admin/ajax/i18n.php
+++ public/admin/ajax/i18n.php
@@ -4,27 +4,35 @@ include_once ADMIN_PATH."lib/ParseXml.class.php";
 include_once ADMIN_PATH."lib/export.php";
 include_once ROOT_PATH."lib/i18n.php";
 
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Expires: Thu, 01-Jan-70 00:00:01 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+header('Pragma: no-cache');
+
 $db = GCApp::getDB();
 
-if(empty($_REQUEST['project'])) {
-	errorJson('Undefined project');
+if (empty($_REQUEST['project'])) {
+    errorJson('Undefined project');
 }
 $project = $_REQUEST['project'];
 
-if(empty($_REQUEST['level'])) {
-	errorJson('Undefined level');
+if (empty($_REQUEST['level'])) {
+    errorJson('Undefined level');
 }
 $level = pg_escape_string($_REQUEST['level']);
 
-if(empty($_REQUEST['p_key'])) {
-	errorJson('Undefined pkey');
+if (empty($_REQUEST['p_key'])) {
+    errorJson('Undefined pkey');
 }
 
 $xml = new ParseXml();
 $xml->LoadFile(PK_FILE);
 $struct=$xml->ToArray();
 
-if(!isset($struct[$level])) errorJson('Invalid level');
+if (!isset($struct[$level])) {
+    errorJson('Invalid level');
+}
 
 $sql = 'delete from '.DB_SCHEMA.'.localization where project_name = :project and i18nf_id = :field_id and pkey_id = :pkey_id';
 $emptyTranslations = $db->prepare($sql);
@@ -33,17 +41,18 @@ $sql = 'insert into '.DB_SCHEMA.'.localization (project_name, i18nf_id, pkey_id,
     values (:project, :field_id, :pkey_value, :lang_id, :translation)';
 $insertTranslation = $db->prepare($sql);
 
-if(isset($_POST['translations'])) {
-	if(!is_array($_POST['translations'])) errorJson('Invalid data');
-	foreach($_POST['translations'] as $fieldId => $translations) {
-		
+if (isset($_POST['translations'])) {
+    if (!is_array($_POST['translations'])) {
+        errorJson('Invalid data');
+    }
+    foreach ($_POST['translations'] as $fieldId => $translations) {
         $emptyTranslations->execute(array(
             'project'=>$project,
             'field_id'=>$fieldId,
             'pkey_id'=>$_REQUEST['p_key']
         ));
-		
-		foreach($translations as $languageId => $translation) {
+        
+        foreach ($translations as $languageId => $translation) {
             $insertTranslation->execute(array(
                 'project'=>$project,
                 'field_id'=>$fieldId,
@@ -51,55 +60,59 @@ if(isset($_POST['translations'])) {
                 'lang_id'=>$languageId,
                 'translation'=>$translation
             ));
-		}
-	}
-	successJson();
-	
+        }
+    }
+    successJson();
 } else {
+    $localization = new GCLocalization($project);
+    $languages = $localization->getLanguages();
+    $defaultLanguageId = $localization->getDefaultLanguageId();
+    $fields = $localization->getI18nFields($level);
+    
+    $responseData = array(
+        'defaultLanguage' => $defaultLanguageId,
+        'languages' => $languages,
+        'fields' => $fields,
+        'translations' => array()
+    );
+    if (empty($fields)) {
+        successJson($responseData);
+    }
 
-	$localization = new GCLocalization($project);
-	$languages = $localization->getLanguages();
-	$defaultLanguageId = $localization->getDefaultLanguageId();
-	$fields = $localization->getI18nFields($level);
-	
-	$responseData = array(
-		'defaultLanguage' => $defaultLanguageId,
-		'languages' => $languages,
-		'fields' => $fields,
-		'translations' => array()
-	);
-	if(empty($fields)) successJson($responseData);
+    $defaultLanguageData = array();
+    
+    $fieldNames = array();
+    foreach ($fields as $fieldId => $field) {
+        array_push($fieldNames, $field['field_name']);
+    }
 
-	$defaultLanguageData = array();
-	
-	$fieldNames = array();
-	foreach($fields as $fieldId => $field) array_push($fieldNames, $field['field_name']);
-
-	$sql = "select ".implode(',',$fieldNames)." from ".DB_SCHEMA.".$level where ".$struct[$level]['pkey']." = :pkey_value";
+    $sql = "select ".implode(',', $fieldNames)." from ".DB_SCHEMA.".$level where ".$struct[$level]['pkey']." = :pkey_value";
     $stmt = $db->prepare($sql);
     $stmt->execute(array(
         'pkey_value'=>$_REQUEST['p_key']
     ));
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
-	foreach($row as $key => $val) {
-		$defaultLanguageData[$key] = $val;
-	}
-	$responseData['translations'][$defaultLanguageId] = $defaultLanguageData;
-	
-	
-	foreach($languages as $languageId => $foo) {
-		if($languageId == $defaultLanguageId) continue;
-		$responseData['translations'][$languageId] = $localization->getTranslationsByFieldName($languageId, $level, $_REQUEST['p_key']);
-	}
-	successJson($responseData);
+    foreach ($row as $key => $val) {
+        $defaultLanguageData[$key] = $val;
+    }
+    $responseData['translations'][$defaultLanguageId] = $defaultLanguageData;
+    
+    
+    foreach ($languages as $languageId => $foo) {
+        if ($languageId == $defaultLanguageId) {
+            continue;
+        }
+        $responseData['translations'][$languageId] = $localization->getTranslationsByFieldName($languageId, $level, $_REQUEST['p_key']);
+    }
+    successJson($responseData);
 }
 
-
-
-function errorJson($error = 'System error') {
-	die(json_encode(array('result'=>'error','error'=>$error)));
+function errorJson($error = 'System error')
+{
+    die(json_encode(array('result'=>'error','error'=>$error)));
 }
 
-function successJson($responseData = array()) {
-	die(json_encode(array('result'=>'ok', 'data'=>$responseData)));
+function successJson($responseData = array())
+{
+    die(json_encode(array('result'=>'ok', 'data'=>$responseData)));
 }
