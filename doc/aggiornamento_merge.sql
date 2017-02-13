df561ce3eaf6f3929a484ca523b9e3bc870e8861
Pietro d'Orio <pietro.dorio@r3-gis.com>
2017-02-13 09:32:33 +0100

    fix aggiornamento merge 3.4.8 if exists

--- doc/aggiornamento_merge.sql
+++ doc/aggiornamento_merge.sql
@@ -1092,7 +1092,7 @@ ALTER TABLE link ALTER COLUMN winh DROP DEFAULT;
 INSERT INTO version (version_name,version_key, version_date) values ('3.4.7', 'author', '2017-01-16');
 
 --fix unique key per combinazione layer_id + relation_id + field_header anzichè  layer_id + field_header. Questa modifica consente alias
-ALTER TABLE gisclient_34.field DROP CONSTRAINT qtfield_unique_key;
+ALTER TABLE gisclient_34.field DROP CONSTRAINT if exists qtfield_unique_key;
 ALTER TABLE gisclient_34.field
   ADD CONSTRAINT qtfield_unique_key UNIQUE(layer_id, relation_id, field_header);
 
