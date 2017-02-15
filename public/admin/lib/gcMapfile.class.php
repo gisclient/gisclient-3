3992229bd3445c391d7e0e6c1821cc9f5d57ea08
Marco Stevan <marco.stevan@r3-gis.com>
2017-02-15 08:38:45 +0100

    fix config mapproxy

--- public/admin/lib/gcMapfile.class.php
+++ public/admin/lib/gcMapfile.class.php
@@ -1161,7 +1161,7 @@ END";
             file_put_contents($wsgiConfigFile, $content);
         }
         
-        $importConfigFile = MAPPROXY_PATH . 'conf/' . $mapName . '.yaml';
+        $importConfigFile = MAPPROXY_PATH . 'conf/' . $this->projectName . '.' . $mapName . '.yaml';
         if (!file_exists($importConfigFile)) {
             file_put_contents($importConfigFile, 'base: [' . $projectDir.$mapName . '.yaml]');
         }
