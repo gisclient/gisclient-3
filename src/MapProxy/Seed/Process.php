f9af0cd934b0549544a021db320289d2b3b2c497
Marco Stevan <marco.stevan@r3-gis.com>
2017-02-16 18:21:07 +0100

    methods for process management

--- src/MapProxy/Seed/Process.php
+++ src/MapProxy/Seed/Process.php
@@ -27,9 +27,9 @@ class Process
         }
     }
 
-    public function startTask(Task $task)
+    private function getCommand(Task $task)
     {
-        $cmdTpl = "%s -f %s -c 1 %s --seed %s > %s 2> %s &";
+        $cmdTpl = "%s -f %s -c 1 %s --seed %s > %s 2> %s & echo $!";
         $cmd = (sprintf(
             $cmdTpl,
             $this->bin,
@@ -39,10 +39,48 @@ class Process
             $task->getLogFile(),
             $task->getErrFile()
         ));
+        
+        return $cmd;
+    }
 
-        echo $cmd;
-        exec($cmd);
+    private function getPID(Task $task)
+    {
+        $result = shell_exec(sprintf('ps x | grep "%s" | grep "%s"', $this->bin, $task->getTaskName()));
+        $r = preg_split("/\n/", $result);
 
-        return $task;
+        for ($i=0; $i < count($r); $i++) {
+            $a = preg_split("/\s+/", trim($r[$i]));
+            if (in_array($this->bin, $a)) {
+                if ($a[4] !== 'sh') {
+                    return (int)$a[0];
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public function startTask(Task $task)
+    {
+        $pid = shell_exec($this->getCommand($task));
+
+        return $pid;
+    }
+
+    public function isRunning(Task $task)
+    {
+        $pid = $this->getPID($task);
+        if ($pid !== false) {
+            return true;
+        }
+        return false;
+    }
+
+    public function stopTask(Task $task)
+    {
+        $pid = $this->getPID($task);
+        if ($pid) {
+            shell_exec(sprintf('kill %d', $pid));
+        }
     }
 }
