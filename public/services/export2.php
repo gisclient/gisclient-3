b77402746715e49874236031921a229c46aaa1f0
root <root@r3bzprd-vmc-app05.r3-gis.com>
2017-07-25 18:24:28 +0200

    export new version

---
+++ public/services/export2.php
@@ -0,0 +1,134 @@
+<?php
+require_once '../../bootstrap.php';
+require_once ROOT_PATH.'lib/ajax.class.php';
+require_once ROOT_PATH.'lib/export.php';
+
+use GisClient\Author\Db;
+use GisClient\Author\Layer;
+
+$ajax = new \GCAjax();
+$auth = new \GCUser();
+$db = new Db();
+
+$inputJSONText = file_get_contents('php://input');
+if (($data = json_decode($inputJSONText, true)) === null) {
+    $data = $_REQUEST;
+}
+
+if (!is_array($data)) {
+    $data = array($data);
+}
+
+$exports = array();
+foreach ($data as $expConf) {
+    if (!isset($exports[$expConf['export_format']])) {
+        $exports[$expConf['export_format']] = array();
+    }
+    $featureType = $expConf['feature_type'];
+    list($layergroupName, $layerName) = explode('.', $featureType);
+
+    $sql = "SELECT layer_id "
+        . " FROM {$db->getParams()['schema']}.layer "
+        . " INNER JOIN {$db->getParams()['schema']}.layergroup using(layergroup_id) "
+        . " WHERE layergroup_name = :layergroup and layer_name = :layer ";
+    $stmt = $db->getDb()->prepare($sql);
+    $stmt->execute(array(
+        'layergroup'=>$layergroupName,
+        'layer'=>$layerName
+    ));
+    $layerId = $stmt->fetchColumn(0);
+
+    $layer = new Layer($layerId);
+    $catalog = $layer->getCatalog();
+    $layerDb = new Db($catalog);
+
+    $columns = array();
+    $layerFields = $layer->getFields();
+    foreach ($expConf['fields'] as $eField) {
+        foreach ($layerFields as $lField) {
+            if ($lField->getName() == $eField['field_name']) {
+                array_push($columns, $eField['field_name'] . ' AS ' . $eField['title']);
+                break;
+            }
+        }
+    }
+
+    $where = 'true';
+    if (isset($expConf['data'])) {
+        $ids = array();
+        foreach ($expConf['data'] as $key) {
+            if (isset($key[$layer->getPrimaryColumn()])) {
+                array_push($ids, $db->getDb()->quote($key[$layer->getPrimaryColumn()]));
+            } else {
+                throw new \Exception("Error: Invalid data, missing primary key", 1);
+            }
+        }
+        $where .= " AND {$layer->getPrimaryColumn()} IN (";
+        $where .= implode(', ', $ids);
+        $where .= ')';
+    } else if (isset($expConf['extent'])) {
+        $extentString = implode(', ', $expConf['extent']);
+        $where = "ST_Intersects({$layer->getGeomColumn()}, ST_MakeEnvelope({$extentString}, {$layer->getGeomSrid()}))";
+    }
+
+    //Create view
+    $viewName = 'export_' . $layer->getTable() . '_' . session_id() . '_' . rand(0, 999999);
+    $sql = "CREATE VIEW public.{$viewName} AS "
+        . " SELECT " . implode(', ', $columns) . ", {$layer->getGeomColumn()}"
+        . " FROM {$layerDb->getParams()['schema']}.{$layer->getTable()}"
+        . " WHERE {$where}";
+
+    $db->getDb()->query($sql);
+
+    $expConf['srid'] = $layer->getGeomSrid();
+
+    array_push($exports[$expConf['export_format']], array(
+        'config' => array(
+            'db' => $layerDb->getParams()['db_name'],
+            'db_instance' => $layerDb->getDb(),
+            'table' => $viewName,
+            'schema' => 'public',
+            'name' => $layer->getName()
+        ),
+        'extras' => $expConf
+    ));
+}
+
+$zipFile = null;
+if (isset($exports['shp'])) {
+    foreach ($exports['shp'] as $exp) {
+        $export = new \GCExport($exp['config']['db_instance'], 'shp');
+        $url = $export->export(array($exp['config']), array(
+            'name' => 'export_shp',
+            'add_to_zip' => $zipFile,
+            'return_url' => true
+        ));
+    }
+}
+
+if (isset($exports['dxf'])) {
+    foreach ($exports['dxf'] as $exp) {
+        $export = new \GCExport($exp['config']['db_instance'], 'dxf');
+        $url = $export->export(array($exp['config']), array(
+            'name' => 'export_dxf',
+            'add_to_zip' => $zipFile,
+            'return_url' => true,
+            'extent' => $exp['extra']['extent'],
+            'srid' => $exp['extra']['srid']
+        ));
+    }
+}
+
+if (isset($exports['xls'])) {
+    foreach ($exports['xls'] as $exp) {
+        $export = new \GCExport($exp['config']['db_instance'], 'xls');
+        $url = $export->export(array($exp['config']), array(
+            'name' => 'export_xls',
+            'add_to_zip' => $zipFile,
+            'return_url' => true
+        ));
+    }
+}
+
+$ajax->success(array('file'=> $url));
+
