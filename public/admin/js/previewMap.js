4145749bbf5ce5a13530a188bcb10abbfd3e1182
anakin2212 <matteo.zero@taulogic.it>
2018-01-11 14:13:26 +0100

    Fixed problem with map preview. Changed window size, temp map file removal during window closure

--- public/admin/js/previewMap.js
+++ public/admin/js/previewMap.js
@@ -5,16 +5,30 @@ $(document).ready(function() {
 	
 	$('#preview_map_dialog').dialog({
 		title: 'Preview map',
-		width: 480,
-		height: 500,
+		width: 775,
+		height: 735,
 		draggable: false,
 		autoOpen: false,
 		open: function() {
 			$(this).empty();
 			var url = previewMapUrl;
-			url += '?layergroup_id='+$('#layergroup').val();
-			$(this).html('<iframe src="'+url+'" width="450" height="450"></iframe>');
-		}
+			url += $('#layer').val() == undefined ? '?layergroup_id='+$('#layergroup').val() : '?layer_id='+$('#layer').val();
+			$(this).html('<iframe src="'+url+'" width="745" height="685"></iframe>');
+		},
+		close: function() {
+          $(this).empty();
+		  var urlC = previewMapUrl;
+		  urlC += $('#layer').val() == undefined ? '?layergroup_id='+$('#layergroup').val() : '?layer_id='+$('#layer').val();
+		  urlC += "&closeWindow=1";
+          $.ajax({
+            async: true,
+            url: urlC,
+            success: function(result,status,xhr) {},
+            error: function(xhr,status,error) {
+              window.alert(error);
+            }
+          });
+        }
 	});
 	$('a[data-action="preview_map"]').show().click(function(event) {
 		event.preventDefault();
--- public/admin/previewmap/index.php
+++ public/admin/previewmap/index.php
@@ -5,56 +5,89 @@ require_once ADMIN_PATH.'lib/gcFeature.class.php';
 require_once ADMIN_PATH.'lib/gcMapfile.class.php';
 require_once ROOT_PATH."lib/i18n.php";
 
-$db = GCApp::getDB();
 
-// FIXME: generate decent error message and return code
-if(empty($_REQUEST['layergroup_id'])) die("Missing required parameter 'layergroup_id'");
-$layergroupId = (int)$_REQUEST['layergroup_id'];
-
-$mapfile = new gcMapfile();
-$mapfile->setTarget("tmp");
-$tmpMap = $mapfile->writeMap("layergroup",$layergroupId);
-
-$sql = "select project_name, theme_name, project_srid, xc, yc, max_extent_scale, layergroup_name, layergroup_title, sld ".
-	" from ".DB_SCHEMA.".project ".
-	" inner join ".DB_SCHEMA.".theme using(project_name) ".
-	" inner join ".DB_SCHEMA.".layergroup using(theme_id) ".
-	" where layergroup_id = ?";
-
-$stmt = $db->prepare($sql);
-$stmt->execute(array($layergroupId));
-
-$mapConfig = $stmt->fetch(PDO::FETCH_ASSOC);
-if(empty($mapConfig['project_srid'])) die('Missing project srid');
-if(empty($mapConfig['xc']) || empty($mapConfig['yc'])) die('Missing project center');
-if(empty($mapConfig['max_extent_scale'])) die('Missing project max extent');
-
-$layerTitle = $mapConfig['layergroup_title'];
-$layerName = $mapConfig['layergroup_name'];
-//$user = new userApps(array());
-//$user->status = true;
-$user = new GCUser();
-$user->setAuthorizedLayers(array('theme_name'=>$mapConfig['theme_name']));
-
-$scales = explode(',',SCALE);
-$resolutions = array();
-foreach($scales as $scale) {
-	if($scale > $mapConfig['max_extent_scale']) continue;
+$layerTitle = "";
+$layerName = "";
+$tmpMap = "";
+$closing = !empty($_REQUEST['closeWindow']);
+if(empty($_REQUEST['layergroup_id']) && empty($_REQUEST['layer_id']))
+  die("Missing required parameter 'layergroup_id' or 'layer_id'");
+else if(!empty($_REQUEST['layergroup_id']))
+  $mapConfig = manageLayerGroupRequest($layerTitle,$layerName,$tmpMap, GCApp::getDB(), $closing);
+else
+  $mapConfig = manageLayerRequest($layerTitle,$layerName,$tmpMap, GCApp::getDB(), $closing);
+if(!$closing) {
+  $user = new GCUser();
+  $user->setAuthorizedLayers(array('theme_name'=>$mapConfig['theme_name']));
+  $scales = explode(',',SCALE);
+  $resolutions = array();
+  foreach($scales as $scale) {
+    if($scale > $mapConfig['max_extent_scale']) continue;
 	array_push($resolutions, $scale / (39.3701*MAP_DPI));
-}
-$maxExtent = array(
-	$mapConfig['xc'] - $resolutions[0] * TILE_SIZE,
+  }
+  $maxExtent = array(
+    $mapConfig['xc'] - $resolutions[0] * TILE_SIZE,
 	$mapConfig['yc'] - $resolutions[0] * TILE_SIZE,
 	$mapConfig['xc'] + $resolutions[0] * TILE_SIZE,
 	$mapConfig['yc'] + $resolutions[0] * TILE_SIZE
-);
+  );
 
-if (!defined('OPENLAYERS')) {
+  if (!defined('OPENLAYERS')) {
 	// FIXME: handle error in more sensible way
 	// this generates an empty page when display_error = Off
 	// handle like the above error check
 	throw new Exception ("constant OPENLAYERS not defined");
+  }
 }
+
+function manageLayerGroupRequest(&$layerTitle, &$layerName, &$tmpMap, $db, $closing) {
+  $layergroupId = (int)$_REQUEST['layergroup_id'];
+  $mapfile = new gcMapfile();
+  $mapfile->setTarget("tmp");
+  $sql = "select project_name, theme_name, project_srid, xc, yc, max_extent_scale, layergroup_name, layergroup_title, sld ".
+	" from ".DB_SCHEMA.".project ".
+	" inner join ".DB_SCHEMA.".theme using(project_name) ".
+	" inner join ".DB_SCHEMA.".layergroup using(theme_id) ".
+	" where layergroup_id = ?";
+  $stmt = $db->prepare($sql);
+  $stmt->execute(array($layergroupId));
+  $mapConfig = $stmt->fetch(PDO::FETCH_ASSOC);
+  if($closing) {
+    $mapfile->_deleteFile($mapConfig['project_name'], $mapConfig['layergroup_name']);
+  } else {
+    $tmpMap = $mapfile->writeMap("layergroup",$layergroupId);
+    if(empty($mapConfig['project_srid'])) die('Missing project srid');
+    if(empty($mapConfig['xc']) || empty($mapConfig['yc'])) die('Missing project center');
+    if(empty($mapConfig['max_extent_scale'])) die('Missing project max extent');
+    $layerTitle = $mapConfig['layergroup_title'];
+    $layerName = $mapConfig['layergroup_name'];
+  }
+  return $mapConfig;
+}
+
+function manageLayerRequest(&$layerTitle, &$layerName, &$tmpMap, $db) {
+  $layerId = (int)$_REQUEST['layer_id'];
+  $mapfile = new gcMapfile();
+  $mapfile->setTarget("tmp");
+  $sql = "select project_name, theme_name, project_srid, xc, yc, max_extent_scale, layergroup_name ,layer_name, layer_title, sld ".
+	" from ".DB_SCHEMA.".project ".
+	" inner join ".DB_SCHEMA.".theme using(project_name) ".
+    " inner join ".DB_SCHEMA.".layergroup using(theme_id) ".
+    " inner join ".DB_SCHEMA.".layer using(layergroup_id) ".
+	" where layer_id = ?";
+  $stmt = $db->prepare($sql);
+  $stmt->execute(array($layerId));
+  $mapConfig = $stmt->fetch(PDO::FETCH_ASSOC);
+  if($closing) {
+    $mapfile->_deleteFile($mapConfig['project_name'], $mapConfig['layer_name']);
+  } else {
+    $tmpMap = $mapfile->writeMap("layer",$layerId);
+    $layerTitle = $mapConfig['layer_title'];
+    $layerName = $mapConfig['layergroup_name'].".".$mapConfig['layer_name'];
+  }
+  return $mapConfig;
+}
+
 ?><!DOCTYPE HTML><html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>Author - Preview Map</title>
@@ -97,8 +130,8 @@ body, html {
 	padding: 0px;
 }
 #map {
-	width: 450px;
-	height: 450px;
+	width: 745px;
+	height: 685px;
 }
 </style>
 </head>
