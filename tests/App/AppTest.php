20969329f9fe746a32d8332b532462fa3d98be67
Daniel Degasperi <daniel.degasperi@r3-gis.com>
2018-01-26 16:09:51 +0100

    add gcapp.class.php to autoload classmap

--- tests/App/AppTest.php
+++ tests/App/AppTest.php
@@ -1,7 +1,6 @@
 <?php
 
 include __DIR__."/../../config/config.db.php";
-include __DIR__."/../../lib/gcapp.class.php";
 
 class AppTest extends PHPUnit_Framework_TestCase {
     public function testConnection() {
