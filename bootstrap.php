d1ec77e36cfd6ba5b714da90db1ebbcb7de801ef
Daniel Degasperi <daniel.degasperi@r3-gis.com>
2018-01-26 15:27:02 +0100

    First autoload before open the config

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,9 @@
 <?php
 
+$loader = require_once(__DIR__ . '/vendor/autoload.php');
+
 $config = __DIR__ . '/config/config.php';
 if (!file_exists($config)) {
-    die ("Manca setup");
+    die("Manca setup");
 }
 require_once($config);
-$loader = require_once(__DIR__ . '/vendor/autoload.php');
