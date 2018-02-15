b942be8bb5563174a14cad9e86e053a6f19f3c92
Daniel Degasperi <daniel.degasperi@r3-gis.com>
2018-02-15 11:26:12 +0100

    Fixed get all saved filters

--- src/Author/Controller/SavedFilterController.php
+++ src/Author/Controller/SavedFilterController.php
@@ -237,10 +237,11 @@ class SavedFilterController
 
         $db = \GCApp::getDB();
         $sql = "
-        SELECT saved_filter.*, layergroup_name||'.'||layer_name AS layer_id 
+        SELECT saved_filter.*, project_name||'.'||layergroup_name||'.'||layer_name AS layer_id 
             FROM ".DB_SCHEMA.".saved_filter 
             INNER JOIN ".DB_SCHEMA.".layer USING(layer_id)
             INNER JOIN ".DB_SCHEMA.".layergroup USING(layergroup_id)
+            INNER JOIN ".DB_SCHEMA.".theme USING(theme_id)
             WHERE mapset_name=? AND (username = ? OR saved_filter_scope = 'all')
         ";
         // TODO: support group scope!!
