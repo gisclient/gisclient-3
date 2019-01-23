984036c21e54eaace0a86055b5c88dece6b80cca
Daniel Degasperi <daniel.degasperi@r3-gis.com>
2019-01-23 14:39:09 +0100

    Load env from .env file during bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,19 @@
 <?php
 
+use Symfony\Component\Dotenv\Dotenv;
+
 $loader = require_once(__DIR__ . '/vendor/autoload.php');
 
-$config = __DIR__ . '/config/config.php';
-if (!file_exists($config)) {
-    die("Manca setup");
+// Load cached env vars if the .env.local.php file exists
+// Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
+if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
+    $_SERVER += $env;
+    $_ENV += $env;
+} elseif (!class_exists(Dotenv::class)) {
+    throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
+} else {
+    // load all the .env files
+    (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
 }
-require_once($config);
+
+require_once(__DIR__ . '/config/config.php');
