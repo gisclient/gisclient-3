f1d9a792c1a7c12e4efa1ba943f3e4d891433246
Daniel Degasperi <daniel.degasperi@r3-gis.com>
2019-01-25 15:24:07 +0100

    Fixed psr2 errors and warnings in SymbolTest

--- tests/Symbol/SymbolTest.php
+++ tests/Symbol/SymbolTest.php
@@ -2,22 +2,25 @@
 
 use GisClient\Author\Symbol;
 
-include_once __DIR__."/../../config/config.php";
+include_once __DIR__."/../../bootstrap.php";
 
-class SymbolTest extends PHPUnit_Framework_TestCase {
-    public function testSymbolList() {
-		$symbol = new Symbol('symbol');
-		$symbolList = $symbol->getList();
+class SymbolTest extends PHPUnit_Framework_TestCase
+{
+    public function testSymbolList()
+    {
+        $symbol = new Symbol('symbol');
+        $symbolList = $symbol->getList();
         $this->assertTrue(count($symbolList) > 0);
     }
-	
-    public function testSymbolImage() {
-		$symbol = new Symbol('symbol');
-		$symbolList = $symbol->getList(true);
-		foreach($symbolList['values'] as $symbolInfo) {
-			$symbol->filter="symbol.symbol_name='{$symbolInfo['symbol']}'";
-			$img = $symbol->createIcon();
-			$this->assertNotNull($img);
-		}
+    
+    public function testSymbolImage()
+    {
+        $symbol = new Symbol('symbol');
+        $symbolList = $symbol->getList(true);
+        foreach ($symbolList['values'] as $symbolInfo) {
+            $symbol->filter="symbol.symbol_name='{$symbolInfo['symbol']}'";
+            $img = $symbol->createIcon();
+            $this->assertNotNull($img);
+        }
     }
 }
