584340ac21d106f4509adf52647aafe12d1a475d
Daniel Degasperi <daniel.degasperi@r3-gis.com>
2019-01-29 10:09:23 +0100

    Fixed bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -6,10 +6,7 @@ $loader = require_once(__DIR__ . '/vendor/autoload.php');
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
-    $_SERVER += $env;
-    $_ENV += $env;
-} elseif (!class_exists(Dotenv::class)) {
+if (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 } elseif (!getenv('AUTHOR_PUBLIC_URL')) {
     // load all the .env files
