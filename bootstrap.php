ee82208b6659d4319327721fcc8ea60bea61598c
Sergio Segala <sergio.segala@r3-gis.com>
2019-10-10 17:15:49 +0200

    Try to load .env file from author root dir

--- bootstrap.php
+++ bootstrap.php
@@ -10,7 +10,7 @@ if (!class_exists(Dotenv::class)) {
     throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 } elseif (!getenv('AUTHOR_PUBLIC_URL')) {
     // load all the .env files
-    (new Dotenv())->load(dirname(__DIR__).'/.env');
+    (new Dotenv())->load(__DIR__.'/.env');
 }
 
 require_once(__DIR__ . '/config/config.php');
