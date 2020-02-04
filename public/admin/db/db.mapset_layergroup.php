939baa2b3a5c09a5349da22a5c0ab6a32d0d88d9
Marco Giraudi (Old Snapo) <mgiraudi@soluzioninrete.it>
2020-02-04 09:14:03 +0100

    Do not autocomplile mapset on save if legend cache is enabled

--- public/admin/db/db.mapset_layergroup.php
+++ public/admin/db/db.mapset_layergroup.php
@@ -1,7 +1,7 @@
 <?php
 $save=new saveData($_POST);
 $p=$save->performAction($p);
-if(!$save->hasErrors && $save->action=="salva"){
+if(!$save->hasErrors && $save->action=="salva" && !defined('LEGEND_CACHE_PATH')){
 	GCAuthor::refreshMapfile($p->parametri['project'],$p->parametri['mapset']);
 }
 ?>
