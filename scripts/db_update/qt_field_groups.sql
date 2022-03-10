b87fc3cd05656217d94348eb69c2d5e02b5320f0
Marco Giraudi (Old Snapo) <mgiraudi@soluzioninrete.it>
2022-03-10 12:20:27 +0100

    Auth on report fields, bugfix on db update script

--- scripts/db_update/qt_field_groups.sql
+++ scripts/db_update/qt_field_groups.sql
@@ -16,3 +16,23 @@ INSERT INTO gisclient_3.form_level(
 INSERT INTO gisclient_3.form_level(
 	id, level, mode, form, order_fld, visible)
 	VALUES (543, 56, 1, 232, 1, 1);
+
+CREATE TABLE IF NOT EXISTS gisclient_3.qt_field_groups
+(
+    qt_field_id integer NOT NULL,
+    groupname character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT qt_field_groups_pkey PRIMARY KEY (qt_field_id, groupname),
+    CONSTRAINT qt_field_groups_qt_field_id_fkey FOREIGN KEY (qt_field_id)
+        REFERENCES gisclient_3.qt_field (qt_field_id) MATCH FULL
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS gisclient_3.qt_field_groups
+    OWNER to postgres;
+
+GRANT SELECT ON TABLE gisclient_3.qt_field_groups TO mapserver;
+
+GRANT ALL ON TABLE gisclient_3.qt_field_groups TO postgres;
