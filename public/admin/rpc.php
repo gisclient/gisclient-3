ecd24a24976bd6d8ecf186b843caf780b7f1de1f
Loris Zanella <loris.zanella@r3gis.com>
2025-03-07 17:42:06 +0100

    Remove some broken code with missing dependencies

--- public/admin/rpc.php
+++ public/admin/rpc.php
@@ -136,44 +136,6 @@ switch ($azione) {
         return;
 
         break;
-    case "getExportObject":
-        $level=$_REQUEST["level"];
-        $project=$_REQUEST["project"];
-        switch ($level) {
-            case 2:
-                $sql="SELECT project_name as id,project_name as name FROM ".DB_SCHEMA.".project WHERE project_name=:project ORDER BY project_name";
-                break;
-            case 5:
-                $sql="SELECT theme_id as id,theme_name as name FROM ".DB_SCHEMA.".theme LEFT JOIN ".DB_SCHEMA.".project using (project_name) WHERE project_name=:project ORDER BY theme_name";
-                break;
-            case 10:
-                $sql="SELECT layergroup_id as id,layergroup_name as name FROM ".DB_SCHEMA.".layergroup LEFT JOIN ".DB_SCHEMA.".theme using (theme_id) LEFT JOIN ".DB_SCHEMA.".project using (project_name) WHERE project_name=:project ORDER BY layergroup_name";
-                break;
-            case 11:
-                break;
-            case 12:
-                break;
-            case 14:
-                break;
-        }
-        try {
-            $stmt = $db->prepare($sql);
-            $stmt->execute(array('project'=>$project));
-            $ris = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            $obj="";
-            $opt[]="['-1','Seleziona un oggetto']";
-            for ($i=0; $i<count($ris); $i++) {
-                $o=$ris[$i];
-                $val=parse_code($o["name"]);
-                //$val=$o["name"];
-                $opt[]="['$o[id]','$val']";
-            }
-            $res="{name:'obj_id', val:[".implode(",", $opt)."]}";
-        } catch (Exception $e) {
-            echo "{error:'Impossibile eseguire la query'}";
-        }
-        
-        break;
     case "request":
         $fk=$_REQUEST["parent_level"]."_id";
         $table=$_REQUEST["level"];
@@ -207,6 +169,7 @@ switch ($azione) {
         }
         break;
     default:
+        echo "{error:'Invalid action'}";
         break;
 }
 header("Content-Type: text/plain; Charset=".CHAR_SET);
